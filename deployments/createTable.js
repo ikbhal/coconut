31ee3e8d80719da6642584a5ca5c81873e558165
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-08-28 08:02:36 +0530

    create table

---
+++ deployments/createTable.js
@@ -0,0 +1,23 @@
+const sqlite3 = require('sqlite3').verbose();
+
+// SQLite database setup
+const db = new sqlite3.Database('coconut_db.db');
+
+// Create the selling_points table if it doesn't exist
+db.run(`CREATE TABLE IF NOT EXISTS selling_points (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    latitude REAL NOT NULL,
+    longitude REAL NOT NULL,
+    address TEXT,
+    name TEXT NOT NULL,
+    mobile_number TEXT NOT NULL,
+    upi TEXT
+)`, (err) => {
+    if (err) {
+        console.error('Error creating table:', err.message);
+    } else {
+        console.log('Table created successfully');
+    }
+    
+    db.close();
+});
